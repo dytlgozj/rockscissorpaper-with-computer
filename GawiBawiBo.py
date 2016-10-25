0583c3b0efc0fdf4798ce636948b5904fd569461
dytlgozj <dytlgozj@naver.com>
2016-10-25 17:23:34 +0900

    modified to repitition

---
+++ GawiBawiBo.py
@@ -0,0 +1,39 @@
+import random
+
+"""
+* Traditional gawi-bawi-bo game
+
+@author Joeng Um Lee
+@created 12-10-2016
+"""
+
+# 0 : gawi, 1. bawi, 2 : bo
+
+def determine_winner(my_hand, com_hand):
+	"""
+	Determine winner of the game.
+	:param my_hand : my hand parameter
+	:param com_hand : predefined computer choice
+	:return None : None is returned. (void)
+	"""
+	a = my_hand - com_hand
+	if a > 0 or a == -2:
+		print "You win"
+	elif a == 0:
+		print "Draw"
+	else:
+		print "You lose"
+
+if __name__ == '__main__':
+	""" Comment """
+	com_hand = random.randint(0, 2)
+
+	while(True):
+		print "Show your hand (0 : gawi, 1 : bawi, 2 : bo)"
+		my_hand = int(raw_input())
+		determine_winner(my_hand, com_hand)
+		print "Play More? (O/X) : "
+		if(raw_input() == 'X'):
+			break
+		else:
+			continue
